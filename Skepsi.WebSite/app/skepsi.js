2b81dd0324539aaf7d495fe8946c8a5a0ff19185
Pedro Pisandelli <pisandelli@gmail.com>
2015-08-02 19:01:36 -0300

    fix #36

--- Skepsi.WebSite/app/skepsi.js
+++ Skepsi.WebSite/app/skepsi.js
@@ -6,8 +6,19 @@
     var angularApp = angular.module('skepsi', []);
 
     // CONTROLLERS
-    angularApp.controller('mainController', ['$scope',
-        function ($scope) {
+    angularApp.controller('mainController', ['$scope', '$http',
+        function ($scope, $http) {
             $scope.name = "Pedro";
-        }]);
+
+            $http.get('http://skepsiteste.azurewebsites.net/api/Pensamentos/Dia')
+                .success(function (result) {
+
+                    $scope.pensamento = result;
+                })
+                .error(function (data, status) {
+                    console.log(data);
+                });
+
+        }
+    ]);
 }());
